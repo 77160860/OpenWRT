a1d7533ccc4ea47a9e6b209992f3b9ea592fd8b0
dep0rt <9974648+77160860@users.noreply.github.com>
2026-09-26 22:11:09 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -72,8 +72,8 @@ UPDATE_PACKAGE "luci-app-gecoosac" "laipeng668/luci-app-gecoosac" "main"
 
 UPDATE_PACKAGE "sing" "77160860/luci-app-sing" "main"
 UPDATE_PACKAGE "sing-box" "77160860/OpenWRT" "main" "pkg"
-UPDATE_PACKAGE "mihomo" "77160860/OpenWRT" "main" "pkg"
 UPDATE_PACKAGE "meta" "77160860/OpenWRT" "main" "pkg"
+UPDATE_PACKAGE "mihomo" "77160860/OpenWRT" "main" "pkg"
 rm -rf ./OpenWrt-nikki/mihomo-meta ./OpenWrt-nikki/mihomo-alpha
 mkdir -p ./OpenWrt-nikki/mihomo-meta
 cp -f "$GITHUB_WORKSPACE/mihomo/Makefile" ./OpenWrt-nikki/mihomo-meta/Makefile
